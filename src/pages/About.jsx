4696783d420d0e29835998bb5696a1bef54458b8
TryCatchB <pozdrakovzena@gmail.com>
2024-10-28 15:08:00 +0300

    Updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,11 +5,15 @@ const About = () => {
         <h2 className="description-title">About my web-site</h2>
         <p className="description">
           To create a Frontend-portfolio website, I used modern technologies
-          such as React for building interfaces, Vite for quick project
-          assembly, as well as HTML and CSS for styling. The project also
-          implements navigation using React Router, data management through
-          React Query, as well as the use of axios to work with APIs. In order
-          to ensure code quality, I have integrated eslint.
+          such as <strong>React</strong> for building interfaces,{" "}
+          <strong>Vite</strong> for quick project assembly, as well as{" "}
+          <strong>HTML</strong> and <strong>CSS</strong> for styling. The
+          project also implements navigation using <strong>React Router</strong>
+          , data management through <strong>React Query</strong>, as well as the
+          use of axios to work with APIs. In order to ensure code quality, I
+          have integrated <strong>eslint</strong>. Also I used{" "}
+          <strong>React-Content-Loader</strong> for displaying loading
+          placeholders while data is being fetched from APIs.
         </p>
       </section>
     </main>
